b056ccbc08d67feeca8b940e187a267256e2dda6
Tarik Alkasab <tarik@alkasab.org>
2025-04-15 16:42:42 -0400

    Brought functionality for findingmodels from other repo.

--- src/findingmodel/__init__.py
+++ src/findingmodel/__init__.py
@@ -1,2 +1,6 @@
-def main() -> None:
-    print("Hello from findingmodel!")
+from . import tools as tools
+from .config import settings as settings
+from .finding_model import FindingModelBase as FindingModelBase
+from .finding_model import FindingModelFull as FindingModelFull
+
+all = ["FindingModelBase", "FindingModelFull", "settings", "tools"]
